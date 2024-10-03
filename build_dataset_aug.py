dbeb9626bb20783b83829c079ad0efa5b685e7fe
Kumar <kumar.1992703@studenti.uniroma1.it>
2024-10-03 13:49:41 +0200

    dataset

--- build_dataset_aug.py
+++ build_dataset_aug.py
@@ -1,14 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 19:51:20 2024
+
+@author: Administrator
+"""
+
+# ------------------------------------------------------------------------
+# LW-DETR
+# Copyright (c) 2024 Baidu. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from Conditional DETR (https://github.com/Atten4Vis/ConditionalDETR)
+# Copyright (c) 2021 Microsoft. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Copied from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# ------------------------------------------------------------------------
+
+import torch
+import torch.utils.data
+import torchvision
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-import torch
-import torchvision
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
-import numpy as np
+from torch.utils.data import DataLoader, DistributedSampler
 import misc
-import cv2
 
 class CocoDetection(torchvision.datasets.CocoDetection):
-    def __init__(self, img_folder, ann_file, transforms=None):
+    def __init__(self, img_folder, ann_file, transforms):
         super(CocoDetection, self).__init__(img_folder, ann_file)
         self._transforms = transforms
         self.prepare = ConvertCoco()
@@ -18,51 +36,24 @@ class CocoDetection(torchvision.datasets.CocoDetection):
         image_id = self.ids[idx]
         target = {'image_id': image_id, 'annotations': target}
         img, target = self.prepare(img, target)
-
-        # Albumentations expects numpy arrays for image and bounding boxes
-        img = np.array(img)
-        h, w = img.shape[:2]
-
-        # Normalize bounding boxes to be in range [0, 1] for Albumentations
-        bboxes = target['boxes'] / torch.tensor([w, h, w, h], dtype=torch.float32)  # Normalize bounding boxes
-        bboxes = bboxes.tolist()
-        class_labels = target['labels'].tolist()
-
         if self._transforms is not None:
-            transformed = self._transforms(
-                image=img,
-                bboxes=bboxes,
-                class_labels=class_labels
-            )
-            img = transformed['image']
-
-            # Normalize bounding boxes to model input
-            bboxes = torch.tensor(transformed['bboxes'], dtype=torch.float32)
-
-            # Ensure bounding boxes are within the normalized range
-            bboxes = bboxes.clamp(min=0, max=1)
-
-            if bboxes.size(0) > 0:  # Only process if there are bounding boxes
-                target['boxes'] = bboxes
-                target['labels'] = torch.tensor(transformed['class_labels'], dtype=torch.int64)
-            else:
-                # Handle the case where there are no bounding boxes
-                target['boxes'] = torch.empty((0, 4), dtype=torch.float32)
-                target['labels'] = torch.empty((0,), dtype=torch.int64)
-
+            img, target = self._transforms(image=img, bboxes=target['boxes'], labels=target['labels'])
         return img, target
 
 
 class ConvertCoco(object):
     def __call__(self, image, target):
         w, h = image.size
+
         image_id = target["image_id"]
         image_id = torch.tensor([image_id])
-        anno = [obj for obj in target["annotations"] if 'iscrowd' not in obj or obj['iscrowd'] == 0]
-        boxes = [obj["bbox"] for obj in anno]
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
 
-        # Convert COCO bounding boxes (x, y, w, h) to (x_min, y_min, x_max, y_max)
+        anno = target["annotations"]
+        anno = [obj for obj in anno if 'iscrowd' not in obj or obj['iscrowd'] == 0]
+
+        boxes = [obj["bbox"] for obj in anno]
+        # guard against no boxes via resizing
+        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
         boxes[:, 2:] += boxes[:, :2]
         boxes[:, 0::2].clamp_(min=0, max=w)
         boxes[:, 1::2].clamp_(min=0, max=h)
@@ -79,11 +70,12 @@ class ConvertCoco(object):
         target["labels"] = classes
         target["image_id"] = image_id
 
+        # for conversion to coco api
         area = torch.tensor([obj["area"] for obj in anno])
         iscrowd = torch.tensor([obj["iscrowd"] if "iscrowd" in obj else 0 for obj in anno])
-
         target["area"] = area[keep]
         target["iscrowd"] = iscrowd[keep]
+
         target["orig_size"] = torch.as_tensor([int(h), int(w)])
         target["size"] = torch.as_tensor([int(h), int(w)])
 
@@ -94,42 +86,91 @@ def make_coco_transforms(image_set):
     if image_set == 'train':
         return A.Compose([
             A.HorizontalFlip(p=0.5),
-            A.RandomResizedCrop(640, 640, scale=(0.8, 1.0), p=0.5),
-            A.RandomBrightnessContrast(p=0.2),
-            A.ColorJitter(p=0.2),
-            A.Resize(640, 640),  # Ensure all images are resized to 640x640
+            A.RandomResizedCrop(height=640, width=640, scale=(0.8, 1.0)),
+            A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            A.GaussNoise(var_limit=(10, 50), p=0.5),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2()
-        ], bbox_params=A.BboxParams(format='coco', label_fields=['class_labels'], min_visibility=0.3))
+        ])
 
     if image_set == 'val':
         return A.Compose([
-            A.Resize(640, 640),  # Resize to 640x640 for validation
+            A.Resize(height=640, width=640),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2()
-        ], bbox_params=A.BboxParams(format='coco', label_fields=['class_labels'], min_visibility=0.3))
+        ])
 
     raise ValueError(f'unknown {image_set}')
 
 
+def make_coco_transforms_square_div_64(image_set):
+    if image_set == 'train':
+        return A.Compose([
+            A.HorizontalFlip(p=0.5),
+            A.RandomResizedCrop(height=640, width=640, scale=(0.8, 1.0)),
+            A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            A.GaussNoise(var_limit=(10, 50), p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2()
+        ])
+    
+    elif image_set == 'val':
+        return A.Compose([
+            A.Resize(height=640, width=640),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2()
+        ])
+
+    raise ValueError(f'unknown {image_set}')
+
+
+def Calculate_class_weights(dataset):
+    labels = []
+    for _, target in dataset:
+        labels.extend(target['labels'].tolist())
+    
+    class_counts = torch.bincount(torch.tensor(labels))
+    class_weights = 1.0 / class_counts.float()
+    
+    # Create sample weights
+    sample_weights = [class_weights[target['labels']].mean() for _, target in dataset]
+    sample_weights = torch.tensor(sample_weights)
+    
+    return sample_weights
+
 def build_dataset(image_folder, ann_file, image_set, batch_size, num_workers, square_div_64=False):
-    dataset = CocoDetection(image_folder, ann_file, transforms=make_coco_transforms(image_set))
+    if square_div_64:
+        dataset = CocoDetection(image_folder, ann_file, transforms=make_coco_transforms_square_div_64(image_set))
+    else:
+        dataset = CocoDetection(image_folder, ann_file, transforms=make_coco_transforms(image_set))
 
     if image_set == 'train':
         drop_last = True
-        sampler = RandomSampler(dataset)
+
+        # Initialize RandomSampler
+        sampler = torch.utils.data.RandomSampler(dataset)
+        
+        # Use the sampler in a DataLoader
         data_loader = DataLoader(dataset, batch_size=batch_size, sampler=sampler,
                                  collate_fn=misc.collate_fn, num_workers=num_workers, drop_last=drop_last, pin_memory=True)
+        
     else:
         drop_last = False
-        sampler_val = SequentialSampler(dataset)
-        data_loader = DataLoader(dataset, batch_size=batch_size, sampler=sampler_val,
-                                 collate_fn=misc.collate_fn, num_workers=num_workers, drop_last=drop_last, pin_memory=True)
+        sampler_val = torch.utils.data.SequentialSampler(dataset)
+        data_loader = DataLoader(dataset, batch_size, sampler=sampler_val, drop_last=drop_last,
+                                 collate_fn=misc.collate_fn, num_workers=num_workers, pin_memory=True)
 
     return data_loader
 
-# Example usage:
-# image_folder = 'path/to/images'
-# ann_file = 'path/to/annotations.json'
-# train_loader = build_dataset(image_folder, ann_file, 'train', batch_size=8, num_workers=4)
-# val_loader = build_dataset(image_folder, ann_file, 'val', batch_size=8, num_workers=4)
+
+"""
+example use
+
+img_folder='/content/traffic_monitoring/coco/train'
+ann_file='/content/traffic_monitoring/coco/train/_annotations.coco.json'
+image_set='val'
+batch_size=8
+num_workers=2
+square_div_64=True
+train_dataset=build_dataset(img_folder, ann_file, image_set,batch_size,num_workers,square_div_64)
+"""
