d070ae07983aeea6c65b1cca299b27591b97ac7b
Kumar <kumar.1992703@studenti.uniroma1.it>
2024-10-08 04:24:37 +0200

    focal loss

--- build_dataset.py
+++ build_dataset.py
@@ -99,7 +99,7 @@ def make_coco_transforms(image_set):
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    scales = [ 640]
 
     if image_set == 'train':
         return T.Compose([
